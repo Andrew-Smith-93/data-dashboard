e0642a062c9938f704ca319001f7a5a37989580d
Andrew-Smith-93 <andrewsmith.independent@gmail.com>
2023-08-21 22:22:58 -0500

    storybook setup

--- src/components/ThemeToggleButton/ThemeToggleButton.tsx
+++ src/components/ThemeToggleButton/ThemeToggleButton.tsx
@@ -1,9 +1,8 @@
 import IconButton from "@mui/material/IconButton";
-import Brightness7Icon from "@mui/icons-material/Brightness7";
-import Brightness4Icon from "@mui/icons-material/Brightness4";
+import LightModeOutlinedIcon from '@mui/icons-material/LightModeOutlined';
+import DarkModeOutlinedIcon from '@mui/icons-material/DarkModeOutlined';
 import React from "react";
 import {useTheme} from "@mui/system";
-import Typography from "@mui/material/Typography";
 import {useMediaQuery} from "@mui/material";
 
 export type ThemeToggleButtonProps = {
@@ -17,11 +16,8 @@ const ThemeToggleButton = (props: ThemeToggleButtonProps) => {
     const colorMode = React.useContext(ColorModeContext);
     return (
         <>
-            {mobileCheck && (
-                <Typography>{theme.palette.mode}</Typography>)
-            }
             <IconButton sx={{mr: 2}} title={theme.palette.mode + ' mode'} aria-label={theme.palette.mode + ' mode button'} onClick={colorMode.toggleColorMode} color="inherit">
-                {theme.palette.mode === 'dark' ? <Brightness7Icon/> : <Brightness4Icon/>}
+                {theme.palette.mode === 'dark' ? <DarkModeOutlinedIcon/> : <LightModeOutlinedIcon/>}
             </IconButton>
         </>)
 }
